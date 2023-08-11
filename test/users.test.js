7c187c59dc98717a359e90f08cb3491ccbb25c21
Alef Ojeda de Oliveira <alef.oliveira@dimensa.com.br>
2023-08-11 17:22:54 -0300

    add faker data and fix tests

--- test/users.test.js
+++ test/users.test.js
@@ -3,6 +3,9 @@ const expect = chai.expect;
 const request = require('supertest');
 const app = require('../index.js');
 const jwt = require('jsonwebtoken');
+const { faker } = require('@faker-js/faker');
+const Users = require('../models/users');
+
 
 require('dotenv').config();
 
@@ -22,9 +25,9 @@ describe('Teste de integração', () => {
 
   it('Deve criar um novo usuário', async () => {
       const user = {
-        name: 'Teste',
-        email: 'teste@teste.com',
-        type: ['ADMIN','DEFAULT'].at(Math.floor(Math.random() * 2))
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        type: faker.word.sample()
       };
 
     request(app)
@@ -39,4 +42,52 @@ describe('Teste de integração', () => {
         expect(res.body).to.have.property('type');
       });
   });
+
+  it('Deve retornar um usuário', async () => {
+    const userId = await Users.findOne({}).then((user) => user._id);
+
+    request(app)
+      .get(`/users/${userId}`)
+      .set('Authorization', Authorization)
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('name');
+        expect(res.body).to.have.property('email');
+        expect(res.body).to.have.property('type');
+      });
+  });
+
+  it('Deve atualizar um usuário', async () => {
+    const userId = await Users.findOne({}).then((user) => user._id);
+
+    const user = {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      type: faker.word.sample()
+    };
+
+    request(app)
+      .put(`/users/${userId}`)
+      .set('Authorization', Authorization)
+      .send(user)
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body).to.be.an('object');
+      }
+    );
+  });
+
+  it('Deve deletar um usuário', async () => {
+    const userId = await Users.findOne({}).then((user) => user._id);
+
+    request(app)
+      .delete(`/users/${userId}`)
+      .set('Authorization', Authorization)
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body).to.be.an('object');
+      });
+  });
+
 });
